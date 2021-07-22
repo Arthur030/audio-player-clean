0cc0405c9d0d1f8f9557662442e378b0aa0a1688
Arthur030 <arthur.olivier30@gmail.com>
2021-07-22 10:56:36 +0200

    cleaner version of player

---
+++ src/tracks.js
@@ -0,0 +1,47 @@
+import image1 from "./assets/images/dancin.jpg";
+import image2 from "./assets/images/davie504.jpg";
+import image3 from "./assets/images/ricardo.jpg";
+import image4 from "./assets/images/shooting-star.jpg";
+import image5 from "./assets/images/theUntold.jpg";
+import audio1 from "./assets/audio/dancin.mp3";
+import audio2 from "./assets/audio/davie504.mp3";
+import audio3 from "./assets/audio/ricardo.mp3";
+import audio4 from "./assets/audio/shooting-star.mp3";
+import audio5 from "./assets/audio/theUntold.mp3";
+
+
+
+
+const tracks = [
+    {
+    title: "Song 1",
+    artist: "Artist 1",
+    img: image1,
+    audio: audio1
+    },
+    {
+    title: "Song 2",
+    artist: "Artist 2",
+    img: image2,
+    audio: audio2
+    },
+    {
+    title: "Song 3",
+    artist: "Artist 3",
+    img: image3,
+    audio: audio3
+    },
+    {
+    title: "Song 4",
+    artist: "Artist 4",
+    img: image4,
+    audio: audio4
+    },
+    {
+    title: "The Untold",
+    artist: "BBC",
+    img: image5,
+    audio: audio5
+    }
+]
+export default tracks;
